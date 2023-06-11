79e9cc7df1a545a03c552427445f3f59224f9638
Andey161qqqw1 <razvezlo201220@gmail.com>
2023-06-11 12:07:33 +0300

    NovDate

--- render.js
+++ render.js
@@ -32,8 +32,8 @@ export function renderAndLogin({ info, addFormName, formLoginValue }) {
       });
     } 
   const infoHtml = info.map((comment) => {
-    const now = comment.date;
-    const createDate = format(now, "yyyy-MM-dd HH:mm:ss");
+    const nowDate = comment.date;
+    const createDate = format(nowDate, "yyyy-MM-dd HH:mm:ss");
     return `<li class="comment">
       <div class="comment-header">
         <div class="name_people">${comment.name}</div>
